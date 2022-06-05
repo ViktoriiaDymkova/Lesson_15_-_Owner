772c8d5d538d8585c8e0757eccc338e39725cd5b
user <stel-vd@yandex.ru>
2022-06-05 23:17:12 +0300

    Added README.md

--- src/test/java/io/qaguru/owner/ApiTest.java
+++ src/test/java/io/qaguru/owner/ApiTest.java
@@ -9,9 +9,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.InstanceOfAssertFactories.PATH;
 
 public class ApiTest {
 
